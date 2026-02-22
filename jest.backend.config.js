a7c03c6bfce4f87b996b68547f8dfb8cd87e7084
Ivan Koo Zhuo Hui <zhuohui.koo@gmail.com>
2026-02-22 21:24:30 +0800

    Fix 'Add to Cart' in Product Details Page (#8)
    * Fix Add to Cart in Product Details page

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -1,27 +1,27 @@
 export default {
-  transform: {},
-  // display name
-  displayName: "backend",
+	transform: {},
+	// display name
+	displayName: "backend",
 
-  // inject jest globals for ES modules
-  injectGlobals: true,
+	// inject jest globals for ES modules
+	injectGlobals: true,
 
-  // when testing backend
-  testEnvironment: "node",
+	// when testing backend
+	testEnvironment: "node",
 
-  // which test to run
-  testMatch: ["<rootDir>/controllers/*.test.js"],
+	// which test to run
+	testMatch: ["<rootDir>/controllers/*.test.js"],
 
-  // jest code coverage
-  collectCoverage: true,
-  collectCoverageFrom: ["controllers/**"],
-  coverageThreshold: {
-    global: {
-      lines: 100,
-      functions: 100,
-    },
-  },
-  moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
-  },
+	// jest code coverage
+	collectCoverage: true,
+	collectCoverageFrom: ["controllers/**"],
+	coverageThreshold: {
+		global: {
+			lines: 100,
+			functions: 100,
+		},
+	},
+	moduleNameMapper: {
+		"^(\\.{1,2}/.*)\\.js$": "$1",
+	},
 };
