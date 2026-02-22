5c5c58a8d1cd5185fed8f1db7dfd46b5af7e891d
smolegz <zhuohui.koo@gmail.com>
2026-02-22 15:10:29 +0800

    Fix bugs

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -18,7 +18,7 @@ const Search = () => {
 					<h6>{values?.results.length < 1 ? "No Products Found" : `Found ${values?.results.length}`}</h6>
 					<div className="d-flex flex-wrap mt-4">
 						{values?.results.map((p) => (
-							<div className="card m-2" style={{ width: "18rem" }}>
+							<div key={p._id} className="card m-2" style={{ width: "18rem" }}>
 								<img src={`/api/v1/product/product-photo/${p._id}`} className="card-img-top" alt={p.name} />
 								<div className="card-body">
 									<h5 className="card-title">{p.name}</h5>
@@ -28,7 +28,7 @@ const Search = () => {
 										More Details
 									</button>
 									<button
-										class="btn btn-secondary ms-1"
+										className="btn btn-secondary ms-1"
 										onClick={() => {
 											setCart([...cart, p]);
 											localStorage.setItem("cart", JSON.stringify([...cart, p]));
